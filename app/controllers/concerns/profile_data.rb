71d231643502380806a8fabdbef6a1a8d2533125
mrlzla <mrlzlamogilev@gmail.com>
2016-11-26 15:47:22 +0300

    Add profile controller and basic views

---
+++ app/controllers/concerns/profile_data.rb
@@ -0,0 +1,18 @@
+module ProfileData
+  extend ActiveSupport::Concern
+  def profile_params
+    params.require(:profile).permit(:first_name,
+                                    :last_name,
+                                    :sex,
+                                    :home_phone,
+                                    :mobile_phone,
+                                    :company_name,
+                                    :job_position,
+                                    :nationality,
+                                    :credit_number,
+                                    :address,
+                                    :salary,
+                                    :family_status,
+                                    :birth_date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   }
 
   resources :building_profiles
+  resource :profile, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
