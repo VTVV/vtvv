03cf2e3da84216ae760caacbb14e389517532aa6
nwtima <tim.ssky@gmail.com>
2016-12-10 22:07:49 +0300

    Borrowers request search

--- app/services/requests_service.rb
+++ app/services/requests_service.rb
@@ -1,11 +1,24 @@
 module RequestsService
 
   def find_appropriate_borrower_requests(investor_request)
-    BorrowerRequest
-        .where(status: :pending)
-        .select do |br|
-          true
-        end
+    if investor_request.due_date < DateTime.now
+      []
+    else
+      lowest_cs = investor_request.from_rate
+      highest_cs = investor_request.to_rate
+      BorrowerRequest
+          .select do |br|
+            ### by ROT
+            cs = br.account.credit_score
+            cs_ok = (cs >= lowest_cs && cs <= highest_cs)
+            ### by status
+            status_ok = (br.status == 'pending')
+            ### by date
+            duration_ok = br.duration < ((investor_request.due_date - DateTime.now) / 1.week)
+            status_ok && cs_ok && duration_ok
+          end
+          .order('amount_cents DESC')
+    end
   end
 
 end
