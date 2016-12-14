2e82b53fb0bec25c5723eeb4f31d4b39c44dcf52
nwtima <tim.ssky@gmail.com>
2016-12-15 01:56:45 +0300

    Dashboards

--- app/controllers/borrower/dashboards_controller.rb
+++ app/controllers/borrower/dashboards_controller.rb
@@ -1,5 +1,15 @@
 class Borrower::DashboardsController < Borrower::ApplicationController
   def show
-    ### TODO STATISTICS
+    @borrower_requests = current_account.borrower_requests.first(10)
+    @requests_count = current_account.borrower_requests.count
+    collect_dollars = Proc.new { |sum, t| sum += t.amount.dollars; sum }
+    @money_borrowed = ArdisTransaction
+                          .where(borrower_id: current_account.id)
+                          .where(kind: :loan)
+                          .reduce(0, &collect_dollars)
+    @money_refunded = ArdisTransaction
+                          .where(borrower_id: current_account.id)
+                          .where(kind: :refund)
+                          .reduce(0, &collect_dollars)
   end
 end
