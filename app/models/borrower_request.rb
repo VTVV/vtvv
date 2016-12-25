6a2a3401f4cf0900e75336eec3fb707b6bdecd5a
nwtima <tim.ssky@gmail.com>
2016-12-25 14:21:55 +0300

    Fix

--- app/models/borrower_request.rb
+++ app/models/borrower_request.rb
@@ -27,7 +27,7 @@ class BorrowerRequest < ApplicationRecord
   def amount_to_complete
     beginning_amount = self.amount
     borrowed_amount = self.debts.reduce(0) do |sum, debt|
-      money = debt.ardis_transactions.where(status: :loan).reduce(0) do |sum, loan|
+      money = debt.ardis_transactions.where(kind: :loan).reduce(0) do |sum, loan|
         sum += loan.amount
         sum
       end
